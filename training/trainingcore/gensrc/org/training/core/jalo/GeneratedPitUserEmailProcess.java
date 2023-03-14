f13b18c74bc0d6ae9fd34c1eb0476681ad8cf191
Anupamata <113500187+Anupamata@users.noreply.github.com>
2023-03-14 14:53:56 +0530

    External api

--- training/trainingcore/gensrc/org/training/core/jalo/GeneratedPitUserEmailProcess.java
+++ training/trainingcore/gensrc/org/training/core/jalo/GeneratedPitUserEmailProcess.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Mar 10, 2023, 4:39:53 PM                    ---
+ * --- Generated at Mar 14, 2023, 2:49:03 PM                    ---
  * ----------------------------------------------------------------
  */
 package org.training.core.jalo;
